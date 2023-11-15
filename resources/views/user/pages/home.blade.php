071fcd1060097df74f2c54964980af88db158640
Alfi Syahrin <sahrin201@gmail.com>
2023-11-15 21:32:13 +0700

    feat(user) add navbar

---
+++ resources/views/user/pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('user.layout.app')
+@section('content')
+    <div style="">
+        <h1>Hello Halaman home untuk user</h1>
+        <button class="btn btn-danger">Klik Me!</button>
+    </div>
+@endsection
