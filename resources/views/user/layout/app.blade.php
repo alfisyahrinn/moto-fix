06c80fa4ac12749118394c99448f694f4ad06278
Alamsyah703 <21082010249@student.upnjatim.ac.id>
2023-11-17 20:09:19 +0700

    fix(navbar): add navbar-login user

--- resources/views/user/layout/app.blade.php
+++ resources/views/user/layout/app.blade.php
@@ -31,11 +31,21 @@
 
 <body>
     <div>
-        @include('user.components.navbar')
+        @guest
+            @include('user.components.navbar')
+        @endguest
 
+        <!-- Main content -->
         <main class="py-4 container">
+            <!-- Include the authenticated user's navigation bar -->
+            @auth
+                @include('user.components.navbar-login')
+            @endauth
             @yield('content')
+
         </main>
+
+
     </div>
 </body>
 
