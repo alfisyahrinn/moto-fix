73dc7e8a1a76f7d956876c03493c91fe7a5c34c0
RijalNdre <rijalandrias@gmail.com>
2023-11-20 20:53:08 +0700

    feat(models): create database relationship

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Supplier extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function product(){
+        return $this->hasMany(Product::class);
+    }
 }
