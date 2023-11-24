3dd9e8413eabfd87723b8a4ebd137567f930d275
Alfi Syahrin <sahrin201@gmail.com>
2023-11-24 21:11:55 +0700

    fix(layuout.app) fix navbar & sidebar in login pages

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">   
+        rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
@@ -36,9 +36,13 @@
 
     <!-- Page Wrapper -->
     <div id="wrapper">
-        @include('admin.components.sidebar')
+        @if (!Request::is('login'))
+            @include('admin.components.sidebar')
+        @endif
         <div id="content-wrapper" class="d-flex flex-column">
-            @include('admin.components.navbar')
+            @unless (Request::is('login'))
+                @include('admin.components.navbar')
+            @endunless
             <div id="content">
                 @yield('content')
             </div>
