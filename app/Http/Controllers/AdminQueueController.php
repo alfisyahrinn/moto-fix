cd73053663184c7dc65dfa8f4b5675b3202b1020
Alfi Syahrin <sahrin201@gmail.com>
2023-11-24 20:36:06 +0700

    fix(admin) fontawesome version

--- app/Http/Controllers/AdminQueueController.php
+++ app/Http/Controllers/AdminQueueController.php
@@ -10,7 +10,7 @@ class AdminQueueController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
+    { 
         return view('admin.pages.queue');
     }
 
